55ecc27f9f8d1cae142a2b755e53e93b1d6f087d
MANewby <97006286+MANewby@users.noreply.github.com>
2022-04-05 11:55:36 -0700

    Add files via upload

---
+++  Write here your multiply-function.js
@@ -0,0 +1,42 @@
+// Write here your multiply-function
+let multiply = (a,b) => a*b;
+
+let modulous = (a,b) => a%b;
+
+//convert string to an array
+function stringToArray(string){
+
+    // code code code
+  let stringArr = string.split(' ')
+  
+    return stringArr;
+  }
+
+  function stringToArray(string){
+
+    // refactor
+   let stringArr = string.split(' ')
+    
+    return stringArr;
+  
+   
+  }
+//COUNT SHEEP when you sleep
+  var countSheep = function (num){
+    let str = "";
+    for(let i = 1; i <= num; i++) { str+= `${i} sheep...`; }
+    return str;
+  }
+//Beginner lost with out a map
+  function maps(x){
+    return x.map(n => n * 2);
+    }
+//decibel scale
+
+const dBScale = intensity => 10 * (12 + Math.log10(intensity));
+
+//multiply
+function multiply(a, b){
+    return a * b
+  }
+    multiply(4,8)
